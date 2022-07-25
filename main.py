288f2c0d089c402269f04d470aad073fb2d3b178
dewmal <dewmal@egreen.io>
2022-07-25 21:57:12 +0530

    Create Chat

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import py_core.py_core
 
 
 async def main():
-    await py_core.py_core.start_ping_service("/ip4/159.223.200.148/tcp/36047")
+    await py_core.start_chat_service()
+    # await py_core.py_core.start_ping_service("/ip4/159.223.200.148/tcp/36047")
 
 
 if __name__ == '__main__':
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,25 @@
 mod service;
+mod chat;
 
 use pyo3::{prelude::*, wrap_pyfunction};
 use service::run_peer;
+use chat::chat;
 
 
 #[pyfunction]
 fn start_ping_service(py: Python<'_>, address: Option<String>) -> PyResult<&PyAny> {
     pyo3_asyncio::tokio::future_into_py(py, async {
         run_peer(address).await;
-        // async_std::task::sleep(std::time::Duration::from_secs(1)).await;
         Ok("DONE".to_string())
     })
-    // pyo3_asyncio::async_std::into_future(run_peer()).await.map(|_| "Started ping service".to_string());
-    // py.allow_threads(service::run_peer);
-    // executor::block_on(service::run_peer());
-    // Ok("DONE".to_string())
+}
+
+#[pyfunction]
+fn start_chat_service(py: Python<'_>, address: Option<String>) -> PyResult<&PyAny> {
+    pyo3_asyncio::tokio::future_into_py(py, async {
+        chat(address).await.unwrap();
+        Ok("DONE".to_string())
+    })
 }
 
 /// Formats the sum of two numbers as string.
@@ -27,6 +32,7 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 #[pymodule]
 fn py_core(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(start_ping_service,m)?)?;
+    m.add_function(wrap_pyfunction!(start_chat_service, m)?)?;
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
 }
