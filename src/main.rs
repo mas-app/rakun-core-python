3c7e145e58b64a5de5b77c694f14e2b6f2d9585b
dewmal <dewmal@egreen.io>
2022-08-02 07:56:32 +0530

    Update main rs

--- src/main.rs
+++ src/main.rs
@@ -87,14 +87,3 @@ fn main() {
         }
     }
 }
-
-// fn main() {
-//     // let agent_service = chat::chat::AgentService::new("AgentOne".to_string(), None);
-//     // agent_service.start();
-//     // agent_service.chat("Hello, world!".to_string());
-//     // agent_service.chat("Hello, world!".to_string());
-//     // agent_service.chat("Hello, world!".to_string());
-//     // agent_service.chat("Hello, world!".to_string());
-//     // agent_service.chat("Hello, world!".to_string());
-//     // agent_service.stop();
-// }
